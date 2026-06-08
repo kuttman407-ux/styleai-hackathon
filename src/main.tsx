2f289cf93300f0e96f35c5b0920ca884e75acf2a
Kutman <kuttman407@gmail.com>
2026-06-08 16:40:06 +0600

    feat: add vercel analits

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights/next'; // Если хочешь отслеживать еще и скорость загрузки
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// Инициализируем аналитику Vercel
+inject();
+injectSpeedInsights();
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+// Рендерим приложение (один раз, как и было изначально)
+createRoot(document.getElementById("root")!).render(<App />);
